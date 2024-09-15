6ca561b2c08401bef0831c027826197a03f24128
WatcharapolxFa <63010870@kmitl.ac.th>
2024-09-15 18:10:47 +0700

    fix:update code

--- learnFlutter/flutterYoutube/myproject/lib/screen/home.dart
+++ learnFlutter/flutterYoutube/myproject/lib/screen/home.dart
@@ -5,18 +5,16 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.red,
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        padding: const EdgeInsets.all(20),
-        child: const Text(
-          "Hello World",
-          style: TextStyle(
-            fontSize: 30,
-            letterSpacing: 3,
-            color: Colors.white,
-          ),
+    return Container(
+      color: Colors.red,
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+      padding: const EdgeInsets.all(20),
+      child: const Text(
+        "Hello World",
+        style: TextStyle(
+          fontSize: 30,
+          letterSpacing: 3,
+          color: Colors.white,
         ),
       ),
     );
